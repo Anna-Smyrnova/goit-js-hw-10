0cba5d1f347e58759673ce577675b9a412694ca4
Anna Smyrnova <anuto4ka777s@gmail.com>
2025-08-06 16:54:41 +0300

    done task #2 HW10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,45 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(formEl.elements.delay.value);
+  const status = formEl.elements.state.value;
+
+  Message(delay, status);
+});
+
+function createPromise(delay, status) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (status === 'fulfilled') {
+        res(delay);
+      } else {
+        rej(delay);
+      }
+    }, delay);
+  });
+}
+
+function Message(delay, status) {
+  createPromise(delay, status)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#0cb13dff',
+        messageColor: '#fff',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#d02727',
+        messageColor: '#fff',
+      });
+    });
+}
